f302a29d9e68697945a82cd1aae32969185e77f6
bogmarkovic <markovicb599@gmail.com>
2018-06-12 12:49:05 +0200

    Add files via upload

---
+++ programs/shell/emailsender.sh
@@ -0,0 +1,16 @@
+﻿$EmailFrom = “markovicb599@gmail.com”
+
+$EmailTo = “andijovic@gmail.com”
+
+$Subject = “Test”
+
+$Body = “Pokusavam preko mog operativnog sistema da posaljem  ovo.Javi mi ako uspe”
+
+$SMTPServer = “gmail.com”
+
+$SMTPClient = New-Object Net.Mail.SmtpClient($SmtpServer, 587)
+
+$SMTPClient.EnableSsl = $true
+
+$SMTPClient.Credentials = New-Object System.Net.NetworkCredential(“gmail.com”, “gmail.com”);
+$SMTPClient.Send($EmailFrom, $EmailTo, $Subject, $Body)
