22721b83983d28e7224eee319122dbe2a1bea1ac
bogmarkovic <markovicb599@gmail.com>
2018-06-12 12:27:49 +0200

    Delete fixip.bat

--- fixip.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cls
-ipconfig /release
-ipconfig /renew
-ipconfig /registerdns
-echo Do you want to show your new IP?(y/n)
-set/p shownewip=
-if %shownewip% == y start whatsip.bat
-if %shownewip% == n exit
